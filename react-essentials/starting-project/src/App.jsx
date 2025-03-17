a364d45a670bf1ca4e78e2c75be6671419f257e3
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-03-17 23:11:44 +0530

    created TabButton component and added onClick handler

--- react-essentials/starting-project/src/App.jsx
+++ react-essentials/starting-project/src/App.jsx
@@ -1,8 +1,13 @@
-import { CoreConcept } from './components/CoreConcept';
-import { Header } from './components/Header';
+import { CoreConcept } from './components/CoreConcept/CoreConcept';
+import { Header } from './components/Header/Header';
+import { TabButton } from './components/TabButton/TabButton';
 import { CORE_CONCEPTS } from './data';
 
 function App() {
+  const handleSelect = (selectedButton) => {
+    console.log('selectedButton', selectedButton);
+  };
+
   return (
     <div>
       <Header />
@@ -14,6 +19,17 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
         </section>
         <h2>Time to get started!</h2>
       </main>
---
+++ react-essentials/starting-project/src/components/TabButton/TabButton.jsx
@@ -0,0 +1,7 @@
+export function TabButton({ children, onSelect }) {
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
