58fe39caa16fd57d8f2f2965ba2edf72e9c15e7e
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-04-07 14:07:57 +0530

    Created reusable Section component

--- react-essentials/src/components/CoreConcepts.jsx
+++ react-essentials/src/components/CoreConcepts.jsx
@@ -1,15 +1,16 @@
 import { CORE_CONCEPTS } from '../data';
 import { CoreConcept } from './CoreConcept/CoreConcept';
+import Section from './Section';
 
 export default function CoreConcepts() {
   return (
-    <section id='core-concepts'>
+    <Section title='Core Concepts' id='core-concepts'>
       <ul>
         {CORE_CONCEPTS?.length &&
           CORE_CONCEPTS.map((conceptItem) => {
             return <CoreConcept key={conceptItem.title} {...conceptItem} />;
           })}
       </ul>
-    </section>
+    </Section>
   );
 }
