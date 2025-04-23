49c50ee705413e3c79ab4296a74d7a6776fd6388
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-04-23 19:14:13 +0530

    added nested styles to styled components

--- react-styling/src/components/AuthInputs.jsx
+++ react-styling/src/components/AuthInputs.jsx
@@ -15,18 +15,33 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${({ invalid }) => (invalid ? '#f87171' : '#6b7280')};
+  color: ${({ $invalid }) => ($invalid ? '#f87171' : '#6b7280')};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: ${({ invalid }) => (invalid ? '#fed2d2' : '#d1d5db')};
-  color: ${({ invalid }) => (invalid ? '#ef4444' : '#374151')};
-  border: 1px solid ${({ invalid }) => (invalid ? '#f73f3f' : 'transparent')};
+  background-color: ${({ $invalid }) => ($invalid ? '#fed2d2' : '#d1d5db')};
+  color: ${({ $invalid }) => ($invalid ? '#ef4444' : '#374151')};
+  border: 1px solid ${({ $invalid }) => ($invalid ? '#f73f3f' : 'transparent')};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
 `;
 
 export default function AuthInputs() {
@@ -53,32 +68,19 @@ export default function AuthInputs() {
     <div id='auth-inputs'>
       <ControlsContainer>
         <p>
-          <Label
-            invalid={
-              emailNotValid
-            } /* className={`label ${emailNotValid ? 'invalid' : ''}`} */
-          >
-            Email
-          </Label>
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
             type='email'
-            // className={emailNotValid ? 'invalid' : ''}
             onChange={(e) => handleInputChange('email', e.target.value)}
-            invalid={emailNotValid}
+            $invalid={emailNotValid}
           />
         </p>
         <p>
-          <Label
-            invalid={passwordNotValid}
-            /* className={`label ${passwordNotValid ? 'invalid' : ''}`} */
-          >
-            Password
-          </Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
             type='password'
-            // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(e) => handleInputChange('password', e.target.value)}
-            invalid={passwordNotValid}
+            $invalid={passwordNotValid}
           />
         </p>
       </ControlsContainer>
@@ -86,9 +88,7 @@ export default function AuthInputs() {
         <button type='button' className='text-button'>
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
