05cacc639712a8aca88211e2a3804060ebd4124b
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-04-24 02:31:21 +0530

    wrapped adding tailwind styles

--- react-styling/src/components/AuthInputs.jsx
+++ react-styling/src/components/AuthInputs.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
+// import { styled } from 'styled-components';
 
 import Button from './Button';
 import Input from './Input';
 
-const ControlsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
+// const ControlsContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -32,8 +32,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id='auth-inputs'>
-      <ControlsContainer>
+    <div
+      id='auth-inputs'
+      className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'
+    >
+      <div className='flex flex-col gap-2 mb-6'>
         <Input
           type='email'
           onChange={(e) => handleInputChange('email', e.target.value)}
@@ -46,9 +49,9 @@ export default function AuthInputs() {
           label='Password'
           $invalid={passwordNotValid}
         />
-      </ControlsContainer>
-      <div className='actions'>
-        <button type='button' className='text-button'>
+      </div>
+      <div className='flex justify-end gap-4'>
+        <button type='button' className='text-amber-400 hover:text-amber-500'>
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
