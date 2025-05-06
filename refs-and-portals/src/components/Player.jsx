f68676fd5ef637d7205a0c66d916264f99f5d023
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-06 20:45:41 +0530

    -with useState and handleChange;

--- refs-and-portals/src/components/Player.jsx
+++ refs-and-portals/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = ({ target: { value } }) => {
+    setSubmitted(false);
+    setPlayerName(value);
+  };
+
+  const handleClick = () => {
+    setSubmitted(true);
+  };
+
   return (
     <section id='player'>
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type='text' />
-        <button>Set Name</button>
+        <input type='text' onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
