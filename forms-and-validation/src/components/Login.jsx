adee160a64f753bfbe71e62a238af99fff897fb8
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-28 21:03:29 +0530

    added handler & onChange for email, password fields;

--- forms-and-validation/src/components/Login.jsx
+++ forms-and-validation/src/components/Login.jsx
@@ -1,17 +1,45 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleValueChange({ target: { value, name } }) {
+    setValues((prevValues) => ({ ...prevValues, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(values);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className='control-row'>
         <div className='control no-margin'>
           <label htmlFor='email'>Email</label>
-          <input id='email' type='email' name='email' />
+          <input
+            id='email'
+            type='email'
+            name='email'
+            onChange={handleValueChange}
+            value={values.email}
+          />
         </div>
 
         <div className='control no-margin'>
           <label htmlFor='password'>Password</label>
-          <input id='password' type='password' name='password' />
+          <input
+            id='password'
+            type='password'
+            name='password'
+            onChange={handleValueChange}
+            value={values.password}
+          />
         </div>
       </div>
 
