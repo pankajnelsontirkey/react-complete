7de9c01e65f9b5195c7e817d244fb25b46db5f5f
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-28 17:03:11 +0530

    added loading state & fallback text;

--- http-requests/src/components/Places.jsx
+++ http-requests/src/components/Places.jsx
@@ -1,10 +1,20 @@
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
+export default function Places({
+  title,
+  places,
+  fallbackText,
+  onSelectPlace,
+  isLoading,
+  loadingText
+}) {
   console.log(places);
 
   return (
     <section className='places-category'>
       <h2>{title}</h2>
-      {places.length === 0 && <p className='fallback-text'>{fallbackText}</p>}
+      {isLoading ? <p className='fallback-text'>{loadingText}</p> : null}
+      {!isLoading && places.length === 0 ? (
+        <p className='fallback-text'>{fallbackText}</p>
+      ) : null}
       {places.length > 0 && (
         <ul className='places'>
           {places.map((place) => (
