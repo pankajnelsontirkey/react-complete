f0c218612e6fa24fdf50fb5f690a3c19dd828135
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-28 17:19:19 +0530

    moved api call to separate function; added sorting places from user location;

--- http-requests/src/components/AvailablePlaces.jsx
+++ http-requests/src/components/AvailablePlaces.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 
 import Places from './Places.jsx';
 import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,19 +15,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const data = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch places!');
-        }
-
-        setAvailablePlaces(data.places);
+        navigator.geolocation.getCurrentPosition(
+          ({ coords: { latitude, longitude } }) => {
+            const sortedPlaces = sortPlacesByDistance(
+              places,
+              latitude,
+              longitude
+            );
+            setAvailablePlaces(sortedPlaces);
+            setIsFetching(false);
+          }
+        );
       } catch (error) {
-        console.log('error');
+        console.log('error', error);
         setError(error);
+        setIsFetching(false);
       }
-      setIsFetching(false);
     }
 
     fetchPlaces();
