18aa990263e33c13b9b72f85a1484cf67bc4b742
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-28 15:18:37 +0530

    used context api in class components;

--- class-components/src/components/UserFinder.jsx
+++ class-components/src/components/UserFinder.jsx
@@ -1,16 +1,12 @@
-import { Fragment } from 'react';
+import { Component, Fragment } from 'react';
 
-import { Component } from 'react';
+import UsersContext from '../store/users-context';
 import classes from './UserFinder.module.css';
 import Users from './Users';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' }
-];
-
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
     this.state = {
@@ -21,13 +17,13 @@ class UserFinder extends Component {
 
   componentDidMount() {
     /* assumer api call is made to fetch users */
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         )
       });
@@ -41,10 +37,12 @@ class UserFinder extends Component {
   render() {
     return (
       <Fragment>
+        {/* <UsersContext.Consumer> */}
         <div className={classes.finder}>
           <input type='search' onChange={this.searchChangeHandler.bind(this)} />
         </div>
         <Users users={this.state.filteredUsers} />
+        {/* </UsersContext.Consumer> */}
       </Fragment>
     );
   }
