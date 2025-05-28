bcf3ecc98888521f3d2af5bc07c3597623cb06ea
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-05-28 16:55:03 +0530

    added api call to fetch places;

--- http-requests/src/components/AvailablePlaces.jsx
+++ http-requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch('http://localhost:3000/places');
+      const data = await response.json();
+      setAvailablePlaces(data.places);
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title='Available Places'
-      places={[]}
+      places={availablePlaces}
       fallbackText='No places available.'
       onSelectPlace={onSelectPlace}
     />
