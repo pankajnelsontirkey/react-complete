87dbefa45d5f108d03a64671d9697faac6a7ccb1
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-06-02 15:06:12 +0530

    added password match validation;

--- forms-and-validation/src/App.jsx
+++ forms-and-validation/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
+import LoginWithState from './components/Login-withState.jsx';
 import Signup from './components/Signup.jsx';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         {/* <Login /> */}
+        {/* <LoginWithState /> */}
         <Signup />
       </main>
     </>
