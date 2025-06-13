a16758aa5d513a30ba9e64152e83cd1046b95a54
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-06-13 21:34:40 +0530

    added cartItem component;

--- projects/food-order-app/src/component/Cart.jsx
+++ projects/food-order-app/src/component/Cart.jsx
@@ -3,11 +3,12 @@ import { useContext } from 'react';
 import CartContext from '../store/CartContext';
 import UserProgressContext from '../store/UserProgressContext';
 import { currencyFormatter } from '../utils/formatting';
+import CartItem from './CartItem';
 import Button from './UI/Button';
 import Modal from './UI/Modal';
 
 export default function Cart() {
-  const { items } = useContext(CartContext);
+  const { items, addItem, removeItem } = useContext(CartContext);
   const { progress, hideCart, showCheckout } = useContext(UserProgressContext);
 
   const cartTotal = items.reduce(
@@ -28,9 +29,14 @@ export default function Cart() {
       <h2>Your Cart</h2>
       <ul>
         {items.map((item) => (
-          <li key={item.id}>
-            {item.name} = {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => addItem(item)}
+            onDecrease={() => removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
