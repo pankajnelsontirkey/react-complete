bc7f14e10fc3a02d15f2224f204f2657406b9452
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-06-18 21:35:56 +0530

    create store and reducers; added store to App component; created increment, decrement buttons in Counter component with dispatchers hooked up;

--- redux/src/components/Counter.jsx
+++ redux/src/components/Counter.jsx
@@ -1,12 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
