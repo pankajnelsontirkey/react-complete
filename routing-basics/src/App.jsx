222ab99dd5807275b886a33c43db8174aee8532a
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-06-24 22:10:11 +0530

    added index path;

--- routing-basics/src/App.jsx
+++ routing-basics/src/App.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: '', element: <Home /> },
+      { index: true, element: <Home /> },
       { path: 'products', element: <ProductsPage /> },
       { path: 'products/:productId', element: <ProductDetailPage /> }
     ]
