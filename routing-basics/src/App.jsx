c50e37d71f6182f1a0d5aa0c8361792b41052916
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-06-24 20:40:13 +0530

    added react-router-dom; added HomePage, ProductsPage;

--- routing-basics/src/App.jsx
+++ routing-basics/src/App.jsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+
+const router = createBrowserRouter([
+  { path: '', element: <Home /> },
+  { path: '/products', element: <ProductsPage /> }
+]);
+
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
