1bf449b03e518ea6036be4584edae75323de7597
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-06-26 20:28:06 +0530

    moved loader function to separate file; created ErrorPage component; useLoaderData to receive data in EventsPage component; used useRouterError() in ErrorPage to get routing/loader function errors; used loader() function to fetch data at events route;

---
+++ routing-practice/src/api/events.js
@@ -0,0 +1,18 @@
+import { json } from 'react-router-dom';
+import { VITE_API_HOST } from '../utils/constants';
+
+export const eventsLoader = async () => {
+  const response = await fetch(`${VITE_API_HOST}/events`);
+
+  if (!response.ok) {
+    // // return { isError: true, message: 'Could not fetch events' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events' }), {
+    //   status: 500
+    // });
+    return json({ message: 'Could not fetch events' }, { status: 500 });
+  } else {
+    // const resData = await response.json();
+    // return resData.events;
+    return response;
+  }
+};
--- routing-practice/src/pages/EventsPage.jsx
+++ routing-practice/src/pages/EventsPage.jsx
@@ -3,7 +3,11 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData();
+  const { events /* , isError, message */ } = useLoaderData();
+
+  // if (isError) {
+  //   return <p>{message}</p>;
+  // }
 
   return (
     <>
