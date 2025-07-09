9926b4cecc81bbb1bce0e0d03f511230f3eb89ff
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-07-09 15:58:54 +0530

    added nodemon to backend; added mutation for adding new events;

--- tanstack-query/src/components/Events/NewEvent.jsx
+++ tanstack-query/src/components/Events/NewEvent.jsx
@@ -1,16 +1,25 @@
+import { useMutation } from '@tanstack/react-query';
 import { Link, useNavigate } from 'react-router-dom';
 
+import { addEvent } from '../../utils/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: addEvent
+  });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
+        {isPending ? 'Submitting...' : null}
         <>
           <Link to='../' className='button-text'>
             Cancel
@@ -20,6 +29,15 @@ export default function NewEvent() {
           </button>
         </>
       </EventForm>
+      {isError ? (
+        <ErrorBlock
+          title='Failed to add event'
+          message={
+            error.info.message ||
+            'Failed to add event, please check inputs and try again.'
+          }
+        />
+      ) : null}
     </Modal>
   );
 }
