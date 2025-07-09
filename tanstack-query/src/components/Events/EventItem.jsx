05bbcb623a0b13958185f023c29276e178d9093b
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-07-09 12:22:49 +0530

    setup starter code for section;

---
+++ tanstack-query/src/components/Events/EventItem.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import { API_HOST } from '../../utils/constants';
+
+export default function EventItem({ event }) {
+  const formattedDate = new Date(event.date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric'
+  });
+  return (
+    <article className='event-item'>
+      <img src={`${API_HOST}/${event.image}`} alt={event.title} />
+      <div className='event-item-content'>
+        <div>
+          <h2>{event.title}</h2>
+          <p className='event-item-date'>{formattedDate}</p>
+          <p className='event-item-location'>{event.location}</p>
+        </div>
+        <p>
+          <Link to={`/events/${event.id}`} className='button'>
+            View Details
+          </Link>
+        </p>
+      </div>
+    </article>
+  );
+}
