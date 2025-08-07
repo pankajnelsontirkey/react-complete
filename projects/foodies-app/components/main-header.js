e17b1b1cc055750739fce8b1588883e497061203
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-08-07 14:26:10 +0530

    added custom component for main-header; added styles for main-header; added main-header to rootLayout;

---
+++ projects/foodies-app/components/main-header.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+import logoImg from '@/assets/logo.png';
+import styles from './main-header.module.css';
+
+export default function MainHeader() {
+  return (
+    <header className={styles.header}>
+      <Link href='/' className={styles.logo}>
+        <img src={logoImg.src} alt='A plate with food on it' />
+        NextLevel Food
+      </Link>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href='/meals'>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href='/community'>Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
