98610d2e09e44acdf6e5ac15d7cd1b2f2fd727c4
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2025-08-07 14:37:24 +0530

    removed header-background styles out from global.css; added main-header-bg component;

--- projects/foodies-app/components/main-header.js
+++ projects/foodies-app/components/main-header.js
@@ -1,26 +1,31 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import logoImg from '@/assets/logo.png';
+import MainHeaderBackground from './main-header-bg';
 import styles from './main-header.module.css';
 
 export default function MainHeader() {
   return (
-    <header className={styles.header}>
-      <Link href='/' className={styles.logo}>
-        <img src={logoImg.src} alt='A plate with food on it' />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link href='/' className={styles.logo}>
+          <Image src={logoImg} alt='A plate with food on it' priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href='/meals'>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href='/community'>Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
